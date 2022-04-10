a794e98159b8e89336bd976ca927db92762ec6de
Con Sınov <hakanunal@devtagon.com>
2022-04-11 01:21:31 +0300

    updated

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,6 @@ export default function Home({ isConnected }) {
           </a>
         </div>
       </main>
-
       <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
